9ac66a1f21312414c46b8027443ffe3ce046c7b7
Uriadutu <balisoan03@gmail.com>
2023-12-14 05:26:29 +0700

    untuk anda

--- klasifikasi/Main.py
+++ klasifikasi/Main.py
@@ -14,6 +14,7 @@ label_encoder = LabelEncoder()
 # Melakukan encoding pada fitur-fitur kategorikal
 categorical_cols = ['age', 'menopause', 'tumor-size', 'inv-nodes', 'node-caps', 'breast', 'breast-quad', 'irradiat']
 
+print(df.head(286))
 for col in categorical_cols:
     df[col] = label_encoder.fit_transform(df[col])
 
@@ -23,25 +24,32 @@ y = df['Class']  # Menggunakan kolom 'Class' sebagai target variabel
 
 # Pembagian Data menjadi set pelatihan dan pengujian
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-x = df.iloc[:,:-1] # Target
-y = df.iloc[:,-1] # Feature
+
 # Range nilai tetangga yang ingin diuji
-neighbors = [3]
+neighbors = [1,3,5,7,9]
+test_sizes = [0.2, 0.3, 0.4]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
-print("X train : ", x_train.shape)
-print("X test : ", x_test.shape)
-print("Y train : ", y_train.shape)
-print("Y test : ", y_test.shape)
-
-for n in neighbors:
+for test_size in test_sizes:
+    train_size = 1 - test_size
+    train_percentage = int(train_size * 100)
+    test_percentage = int(test_size * 100)
+    
+    print(f"{train_percentage}/{test_percentage}:")
+    
+    # Pembagian Data menjadi set pelatihan dan pengujian
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+    
     start_time = time.time()  # Waktu awal proses
-    knn = KNeighborsClassifier(n_neighbors=n)
-    knn.fit(X_train, y_train)
-    y_pred = knn.predict(X_test)
-    accuracy = accuracy_score(y_test, y_pred)
-    end_time = time.time()  # Waktu akhir proses
-    execution_time = end_time - start_time  # Menghitung waktu eksekusi
-    execution_time_rounded = round(execution_time, 3)  # Membulatkan waktu ke tiga angka di belakang koma
-    accuracy_rounded = round(accuracy, 3)  # Membulatkan akurasi ke tiga angka di belakang koma
-    print(f"Neighbors: {n}\nAccuracy: {accuracy_rounded}\nExecution Time: {execution_time_rounded} seconds \n")
+    
+    for n in neighbors:
+        knn = KNeighborsClassifier(n_neighbors=n)
+        knn.fit(X_train, y_train)
+        y_pred = knn.predict(X_test)
+        accuracy = accuracy_score(y_test, y_pred)
+        
+        end_time = time.time()  # Waktu akhir proses
+        execution_time = end_time - start_time  # Menghitung waktu eksekusi
+        execution_time_rounded = round(execution_time, 3)  # Membulatkan waktu ke tiga angka di belakang koma
+        accuracy_rounded = round(accuracy, 3)  # Membulatkan akurasi ke tiga angka di belakang koma
+        
+        print(f"\tNilai K: {n}\n\tAccuracy: {accuracy_rounded}\n\tExecution Time: {execution_time_rounded} seconds\n")
